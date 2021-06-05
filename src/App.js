d6fc07620e97a5ebe1f08b8a42bbef93beb7b81a
sinaaghdasiyekta <sina.aghdasiyekta@gmail.com>
2021-06-05 20:02:19 +0430

    add and import images

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import React from 'react'
 import './test.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import girlPic from './images/girl.png'
+import boyPic from './images/boy.png'
+import shuttlePic from './images/Shuttle.svg'
+import worldPic from './images/connected_world.svg'
+import rockPic from './images/rock.svg'
+import astroidPic from './images/asteroid.svg'
+import earth from './images/earth.svg'
+import moon from './images/moon.svg'
+import saturn from './images/saturn.svg'
+import jupiter from './images/jupiter.svg'
+
 import ArrowButton from './components/Buttons/ArrowButton'
 import CloseButton from './components/Buttons/CloseButton'
-
 import { Button } from 'bootstrap'
 import Footer from './components/footer/Footer';
 import { Nav, Navbar } from 'react-bootstrap'
 
 const App = () => {
-
     return (
     <>
-        {/* <ArrowButton id="leftArrowIcon" parentId="leftArrowParent"/>
+        <ArrowButton id="leftArrowIcon" parentId="leftArrowParent"/>
         <div id="container">
             <header></header>
             <main></main>
             <footer></footer>
         </div>
-        <ArrowButton id="rightArrowIcon" parentId="rightArrowParent"/> */}
+        <ArrowButton id="rightArrowIcon" parentId="rightArrowParent"/>
             <Footer />
     </>
     )
